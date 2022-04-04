e2f60fd81a12da15aa6564226c633a7108cf3232
ryukyunghye <ryukyung123@naver.com>
2022-04-04 21:24:29 +0900

    ryukyunghye: 초보자_001-020

---
+++ ryukyunghye/beginner11_20.py
@@ -0,0 +1,54 @@
+# 11 변수 사용하기 
+samsung = 50000
+total = samsung * 10
+print("총 평가금액 : ", total)
+
+# 12 변수 사용하기
+total = 298000000000000
+now = 50000
+per = 15.79
+print("시가 총액 : ", type(total))
+print("현재가 : ", type(now))
+print("PER : ", type(per))
+
+# 13 문자열 출력
+s = "hello"
+t = "python"
+print(s+"!", t)
+print(s+"! "+t)
+
+# 14 파이썬을 이용한 값 계산
+print(2+2*3) # 8
+
+# 15 type 함수
+a ="132"
+print(type(a))
+
+# 16 문자열을 정수로 변환
+num_str="720"
+num = int(num_str)
+print(num)
+print(int(num_str))
+
+# 17 정수를 문자열 100으로 변환
+num = 100
+sNum = str(num)
+print(sNum)
+print(str(num))
+
+# 18 문자열을 실수로 변환
+num = "15.79"
+fNum= float(num)
+print(fNum)
+print(float(num))
+
+# 19 문자열을 정수로 변환
+year = "2020"
+print(int(year)+1) # 2021
+print(int(year)+2) # 2022
+print(int(year)+3) # 2023
+
+# 20 파이썬 계산
+month = 48584
+total = month *36
+print(total)
