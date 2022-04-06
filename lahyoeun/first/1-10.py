4e4ad7091cd0d80a6ed709d17a1ef19a350c4b9a
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-04-06 19:31:05 +0900

    lahyoeun 초보자1~10

---
+++ lahyoeun/first/1-10.py
@@ -0,0 +1,67 @@
+'''
+001
+화면에 Hello World 문자열을 출력하세요
+'''
+print('Hello World')
+
+'''
+002
+화면에 Mary's cosmetics을 출력하세요.(중간에 '가 있음에 주의하세요)
+'''
+print("Mary's cosmetics")
+
+'''
+003
+화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요)
+신씨가 소리질렀다. "도둑이야".
+'''
+print('신씨가 소리질렀다. "도둑이야".')
+
+'''
+004
+화면에 "C:\Windows"를 출력하세요.
+'''
+print("C:\Windows")
+
+'''
+005
+다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
+print("안녕하세요. \n 만나서\t\t반갑습니다.")
+'''
+# \n은 개행이며, \t는 탭을 의미한다.
+
+'''
+006
+print 함수에 두 개의 단어를 입력한 예제입니다. 아래 코드의 출력 결과를 예상해봅시다.
+예제 : print("오늘은", "일요일")
+'''
+# 오늘은 일요일
+
+'''
+007
+print() 함수를 사용하여 다음과 같이 출력하세요.
+naver;kakao;sk;samsung
+'''
+print("naver;kakao;sk;samsong") #print("naver" ,"kakao" ,"sk" ,"samsung" ,sep=";")
+
+'''
+008
+print() 함수를 사용하여 다음과 같이 출력하세요.
+naver/kakao/sk/samsung
+'''
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+'''
+009
+다음 코드를 수정하여 줄바꿈이 없이 출력하세요.
+(힌트: end='') print 함수는 두 번 사용합니다.
+세미콜론 (;)은 한줄에 여러 개의 명령을 작성하기 위해 사용합니다.
+예제 print("first");print("second")
+'''
+print("first", end="");print("second")
+
+'''
+010
+5/3의 결과를 화면에 출력하세요
+'''
+print(5/3)
