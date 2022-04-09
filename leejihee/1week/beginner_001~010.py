c5875925f3fa6b83b8608c737069d999feb6e4ff
leejihee <jihee6986@naver.com>
2022-04-10 00:08:20 +0900

    이지희 1주차 041~050

--- leejihee/1week/beginner_001~010.py
+++ leejihee/1week/beginner_001~010.py
@@ -37,7 +37,7 @@ print("naver", "kakao", "sk", "samsung", sep="/")
 
 # 009 print 줄바꿈
 # 다음 코드를 수정하여 줄바꿈이 없이 출력하세요. (힌트: end='') print 함수는 두 번 사용합니다. 세미콜론 (;)은 한줄에 여러 개의 명령을 작성하기 위해 사용합니다.
-print("first" end="")
+print("first", end="")
 print("second")
 
 # 010 연산 결과 출력
