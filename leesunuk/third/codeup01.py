b679c14a62f30a70239f0a3cdd2b0c04607ffe33
leesunuk <leesunook12@naver.com>
2022-05-10 00:16:12 +0900

    leesunuk : codeup 02

--- leesunuk/third/codeup01.py
+++ leesunuk/third/codeup01.py
@@ -2,3 +2,60 @@
 1번: Hello 출력하기
 '''
 print("Hello")
+
+'''
+2번: Hello World 출력하기
+'''
+print("Hello World")
+
+'''
+3번: 두줄에 걸쳐 출력하기 
+Hello 
+World
+'''
+print("Hello")
+print("World")
+
+'''
+다른 방법
+'''
+print('''\
+Hello
+World
+''')
+
+'''
+4번: 'Hello' 출력하기
+'''
+print('\'Hello\'')
+
+'''
+다른 방법
+'''
+print("'Hello'")
+
+'''
+5번:"Hello World" 출력하기
+'''
+print("\"Hello\"")
+
+'''
+6번:"!@#$%^&*()" 출력하기
+'''
+print('"!@#$%^&*()"')
+
+'''
+7번: "C:\Download\hello.cpp" 출력하기
+'''
+print("\"C:\Download\hello.cpp\"")
+
+'''
+8번: 출력하기
+┌┬┐
+├┼┤
+└┴┘ 
+'''
+print('''\
+┌┬┐
+├┼┤
+└┴┘''')
