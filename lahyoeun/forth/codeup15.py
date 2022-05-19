89c46b6189b73c3fa01328d96faa1bf1ee3443e8
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-05-19 09:29:35 +0900

    lahyoeun_codeup15

--- lahyoeun/forth/codeup15.py
+++ lahyoeun/forth/codeup15.py
@@ -31,3 +31,14 @@ ps. "내 미래"가 해당 문제 이름입니다. 참고로 전 다녀왔습니
 0 0 0 0 0
 0 0 0 1 0
 '''
+cur = tuple(map(int, input().split()))
+lf, rgt, up, down = map(int, input().split())
+coords = [[0 for _ in range(5)] for _ in range(5)]
+
+move = (cur[0] - up + down, cur[1] - lf + rgt)
+coords[move[0] - 1][move[1] - 1] = 1
+
+for crd in coords:
+    print(*crd)
+
+# 모르겠어서 강의 봄
