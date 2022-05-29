f24e002edaa7029d318cc7d39b480d3f9e0855ab
hyesu02 <102026200+hyesu0203@users.noreply.github.com>
2022-05-29 21:07:05 +0900

    hyesu: greedy01 수정

--- hyesu/5week/greedy01.py
+++ hyesu/5week/greedy01.py
@@ -1,4 +1,4 @@
-# 문제1. 거스름
+# 문제1. 거스름돈
 
 money = int(input())
 count = 0
@@ -6,7 +6,7 @@ count = 0
 coin = [500, 100, 50, 10]
 
 for c in coin:
-    count += money // coin # 거슬러 줄 수 있는 동전의 개수 
+    count += money // coin  # 거슬러 줄 수 있는 동전의 개수
     money %= coin
 
 print(count)
