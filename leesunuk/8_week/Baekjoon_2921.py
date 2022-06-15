9bf69a53b435f28d4023a6cdf152b4022382e312
leesunuk <leesunook12@naver.com>
2022-06-15 19:17:19 +0900

    leesunuk : baekjoon_2921 edit

--- leesunuk/8_week/Baekjoon_2921.py
+++ leesunuk/8_week/Baekjoon_2921.py
@@ -1,9 +1,10 @@
 # 도미노
 
 n = int(input())
-result = 0
+result, s = 0, 0
 for i in range(0, n+1):
-    for j in range(0, n+1, 2):
+    for j in range(s, n+1):
         result += i+j
+    s += 1
 
 print(result)
