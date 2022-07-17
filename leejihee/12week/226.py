a4598fde71b156fe52b1b2f198bdb8f0aae39fcc
leejihee <jihee6986@naver.com>
2022-07-17 20:35:12 +0900

    leejihee: 개미, 바닥, 화폐

---
+++ leejihee/12week/226.py
@@ -0,0 +1,13 @@
+# 5. 효율적인 화폐 구성
+n, m = map(int, input().split())
+k = list([int(input()) for _ in range(n)])
+
+d = [10001]*(m+1)
+
+d[0] = 0
+for i in k:  # 화폐 종류마다 반복
+    for j in range(i, m+1):  # 화폐가 3이면 3부터 셀 수 있도록
+        if d[j-i] != 10001:  # d[j-i]값이 바뀌었다면
+            d[j] = min(d[j], d[j-i]+1)
+
+print(d[m])
