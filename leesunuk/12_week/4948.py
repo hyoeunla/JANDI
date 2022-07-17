2db23b60cc30adefbe90544055d8049d14fcd576
leesunuk <leesunook12@naver.com>
2022-07-17 22:23:38 +0900

    leesunuk : 4948

---
+++ leesunuk/12_week/4948.py
@@ -0,0 +1,21 @@
+# 베르트랑 공준
+
+while 1:
+    n = int(input())
+    if (n != 0 and n > 1):
+        result_sub = 0
+        result = []
+        for i in range(n+1, n*2+1):
+            for j in range(2, n):
+                if i % j == 0:
+                    result_sub = 0
+                    break
+                else:
+                    result_sub = i
+            if result_sub != 0:
+                result.append(result_sub)
+        print(len(result))
+    elif n == 1:
+        print(n)
+    else:
+        break
