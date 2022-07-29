ae74a1327222fd94a10e83bc78ef4d7ce385b43a
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-07-29 19:44:03 +0900

    서울에서 김서방찾기

--- lahyoeun/14week/서울에서_김서방찾기.py
+++ lahyoeun/14week/서울에서_김서방찾기.py
@@ -1,5 +1,5 @@
 def solution(seoul):
-    for i in seoul:
-        if i == "Kim":
-            answer = i
+    for i in range(len(seoul)):
+        if seoul[i] == "Kim":
+            answer = "김서방은 "+str(i)+"에 있다"
     return answer
