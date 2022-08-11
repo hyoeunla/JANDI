39d09861c80c3f737e26ca95cd85b0b241b406c2
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-08-11 23:40:27 +0900

    lahyoeun_1259_all

--- lahyoeun/16week/1259.py
+++ lahyoeun/16week/1259.py
@@ -1,3 +1,4 @@
+# 1번 방법
 n = list(input())
 while n != ['0']:
     if n == n[::-1]:
@@ -5,3 +6,25 @@ while n != ['0']:
     else:
         print("no")
     n = list(input())
+
+# 2번 방법
+n = int(input())
+n1 = n
+n_list = list(map(int, str(n)))
+array = []
+while n != 0:
+    i = n1 % 10
+    n1 = n1//10
+    array.append(i)
+    if n1 == 0 and n_list == array:
+        print('yes')
+        n = int(input())
+        n1 = n
+        array = []
+        n_list = list(map(int, str(n)))
+    elif n1 == 0 and n_list != array:
+        print('no')
+        n = int(input())
+        n1 = n
+        array = []
+        n_list = list(map(int, str(n)))
