a7442307cb01d86a53a65b6b06ab23b02a0e15ca
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-08-12 23:26:38 +0900

    lahyoeun_8958

--- lahyoeun/16week/8958.py
+++ lahyoeun/16week/8958.py
@@ -1,5 +1,14 @@
 n = int(input())
 hap = 0
+k = 0
 for i in range(n):
     dap = list(input())
-    for j in range(len(dap)):
+    for j in dap:
+        if j == "O":
+            k += 1
+            hap += k
+        else:
+            k = 0
+    print(hap)
+    hap = 0
+    k = 0
