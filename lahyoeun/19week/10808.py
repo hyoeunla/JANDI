732822498637e5382f76ee793e8d29a4da4285a7
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-08-31 23:08:35 +0900

    lahyoeun_10808

---
+++ lahyoeun/19week/10808.py
@@ -0,0 +1,57 @@
+S = list(input())
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+for si in S:
+    if si == 'a':
+        a += 1
+    elif si == 'b':
+        b += 1
+    elif si == 'c':
+        c += 1
+    elif si == 'd':
+        d += 1
+    elif si == 'e':
+        e += 1
+    elif si == 'f':
+        f += 1
+    elif si == 'g':
+        g += 1
+    elif si == 'h':
+        h += 1
+    elif si == 'i':
+        i += 1
+    elif si == 'j':
+        j += 1
+    elif si == 'k':
+        k += 1
+    elif si == 'l':
+        l += 1
+    elif si == 'm':
+        m += 1
+    elif si == 'n':
+        n += 1
+    elif si == 'o':
+        o += 1
+    elif si == 'p':
+        p += 1
+    elif si == 'q':
+        q += 1
+    elif si == 'r':
+        r += 1
+    elif si == 's':
+        s += 1
+    elif si == 't':
+        t += 1
+    elif si == 'u':
+        u += 1
+    elif si == 'v':
+        v += 1
+    elif si == 'w':
+        w += 1
+    elif si == 'x':
+        x += 1
+    elif si == 'y':
+        y += 1
+    elif si == 'z':
+        z += 1
+print(a, b, c, d, e, f, g, h, i, j, k, l, m,
+      n, o, p, q, r, s, t, u, v, w, x, y, z)
